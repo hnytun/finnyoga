b7297d34a8e60be6e52805813b9073685ad75e08
= <hnytun@gmail.com>
2019-02-22 12:35:58 +0100

    changed home text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    Velkommen til finnyoga, verdens beste app
+    Velkommen til finnyoga, verdens beste app, laget av henrik og erlend
     <v-btn to="/dropins" large color="primary" round>Finn din neste yogatime</v-btn>
   </div>
 </template>
