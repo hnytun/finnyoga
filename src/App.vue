6fd2e0fb32b1e881277e4da627509c6e3b846e97
= <hnytun@gmail.com>
2019-02-22 13:02:19 +0100

    made user data

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,36 @@
 
 <script>
 
+export default {
+  
+  data () {
+    
+    return {
+      user: {
+        user_id: null,
+        user_name: null,
+        instructor: null,
+        instructorDescription: null,
+        studioName: null,
+        studioAddress: null,
+        studioSuburb: null,
+        //total room for users
+        studioCapacity: null,
+        //booked spots
+        studioType: null,
+      },
+      dropin: {
+        date: null,
+        price: null,
+        difficulty: null,
+        yogaType: null,
+        bookedSpots: null,
+        
+      }
+      
+    }
+  }
+
+}
+
 </script>
